04986562bfef428effebdd59dfa571442d10364f
Hanif Warren <Hanif.warren@gmail.com>
2024-04-02 21:44:12 -0400

    v1.0 submitted to conifer -cleaned up unused code

--- Conifer-weather-alerts/Controllers/HomeController.cs
+++ Conifer-weather-alerts/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace Conifer_weather_alerts.Controllers
     public class HomeController : Controller
     {
         private readonly GeoService _geoService;
-        private object apiResult;
+        
 
         public HomeController(GeoService geoService)
         {
@@ -21,20 +21,9 @@ namespace Conifer_weather_alerts.Controllers
             return View(zones);
         }
 
-        public IActionResult ViewZones(string parameter)
-        {
-            // Call the API with the parameter
-            // Get the result from the API
-
-            // Pass the result to the Zones page
-            return RedirectToAction("Zones", new { result = apiResult });
-        }
-
         public IActionResult Zones(int selectedRegionId)
         {
             List<StateZonesViewModel> StatesandZones = _geoService.GetStatesAndCitiesByRegionId(selectedRegionId);
-            // Display the result on the Zones page
-            ViewData["Result"] = selectedRegionId;
             return View(StatesandZones);
         }
     }
